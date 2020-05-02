e3810061b267028c893ef53ec997912adb6861c8
evzpav <evzpav@gmail.com>
2020-05-02 08:43:01 -0300

    Fix fallback route on express

--- server/router/router.js
+++ server/router/router.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 
+const buildFilePath = "../../vue-client/build";
+
 module.exports = (app, Controller) => {
   app.use("/", express.static(path.join(__dirname, "../../vue-client/build")));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, buildFilePath + "/index.html")));
   app.get("/api/movie/upcoming", Controller.getUpcomingMovies);
   app.get("/api/movie/:id", Controller.getMovieDetails);
 };
