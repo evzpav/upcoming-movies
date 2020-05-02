64630eb959e3cb7fd8a802a9689bc95bbf545d58
evzpav <evzpav>
2020-05-02 09:20:39 -0300

    Fix route

--- server/router/router.js
+++ server/router/router.js
@@ -5,7 +5,7 @@ const buildFilePath = "../../vue-client/build";
 
 module.exports = (app, Controller) => {
   app.use("/", express.static(path.join(__dirname, "../../vue-client/build")));
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, buildFilePath + "/index.html")));
   app.get("/api/movie/upcoming", Controller.getUpcomingMovies);
   app.get("/api/movie/:id", Controller.getMovieDetails);
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, buildFilePath + "/index.html")));
 };
